21511fd7b227bb1801e2f0e3878057cb58f29883
AmberLiao-Penang <79181138+AmberLiao-Penang@users.noreply.github.com>
2022-01-12 19:21:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -317,7 +317,7 @@ def getImageMessage(originalContentUrl):
 
 
 def replyMessage(payload):
-     response = requests.post('https://api.line.me/v2/bot/message/reply',
+    response = requests.post('https://api.line.me/v2/bot/message/reply',
                              headers=HEADER, data=json.dumps(payload))
     print(response.text)
     return 'OK'
